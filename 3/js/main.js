d741716230cccfe21dc6a6614582e8d11ba9b379
Keks <keks@htmlacademy.ru>
2022-10-04 21:44:50 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,16 @@
+function getRandomPositiveNumber (a, b) {
+  if ( a < 0 || b < 0 ) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a,b));
+  const upper = Math.floor(Math.max(a,b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+function checkStringLenght (string, length) {
+  return string,length <= length;
+}
+
+getRandomPositiveNumber();
+checkStringLenght();
+
