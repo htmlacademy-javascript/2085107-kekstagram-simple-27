97b0a966d86f00c752307d2eabb08235b82ef0a6
Keks <keks@htmlacademy.ru>
2022-11-10 23:41:31 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,8 @@
+import './util.js';
+import './data.js';
+import './similar-list.js';
+import './user-modal.js';
+import './user-form.js';
+import { similarPicture } from './data.js';
+
+similarPicture();
