01c88b47fd90a4eb986bdc0e133cfa8ba4a11442
Keks <keks@htmlacademy.ru>
2022-11-17 22:13:05 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,10 @@
+import './util.js';
+import './data.js';
+import './similar-list.js';
+import './user-modal.js';
+import './user-form.js';
+import './scale.js';
+import './effect.js';
+import {similarPicture} from './data.js';
+
+similarPicture();
---
+++ 8/js/user-modal.js
@@ -0,0 +1,45 @@
+import { renderSimilarList,clearSimilarList } from './similar-list.js';
+import { resetScale } from './scale.js';
+import { resetEffects } from './effect.js';
+
+const userModalElement = document.querySelector('body');
+const userModalOpenElement = document.querySelector('.img-upload__input');
+const userModalOpenOverlay = document.querySelector('.img-upload__overlay');
+const userModalCloseElement = document.querySelector('.img-upload__cancel');
+const userForm = document.querySelector('.img-upload__form');
+const onPopupEscKeyDown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeUserModal();
+  }
+};
+
+function openUserModal () {
+  userModalElement.classList.add('modal-open');
+  userModalOpenOverlay.classList.remove('hidden');
+  renderSimilarList();
+  document.addEventListener('keydown', onPopupEscKeyDown);
+}
+
+
+function closeUserModal () {
+  userModalElement.classList.remove('modal-open');
+  userModalOpenOverlay.classList.add('hidden');
+  userForm.reset();
+  resetScale();
+  resetEffects();
+
+
+  clearSimilarList();
+  document.removeEventListener('keydown', onPopupEscKeyDown);
+}
+
+userModalOpenElement.addEventListener('click',() => {
+  openUserModal();
+});
+
+userModalCloseElement.addEventListener('click', () => {
+  closeUserModal();
+});
+
+
